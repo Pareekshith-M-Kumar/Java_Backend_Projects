03e44435fed186dbf914dcbb5bdb5126f3a72e1a
Pareekshith M Kumar <learncoding24712@gmail.com>
2026-04-21 21:51:34 +0530

    Delete created connection.sql

--- created connection.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER 'hiber_user'@'localhost'
-IDENTIFIED BY 'hiber@123';
-GRANT ALL PRIVILEGES
-ON car_db.*
-TO 'hiber_user'@'localhost';
-FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'hiber_user'@'localhost';
